04724ec75230dd5eb11a8dcc1689dfe77bf110b2
Lukas Wala <lukas@pop-os.localdomain>
2023-05-07 16:54:59 +0200

    added tmux.sh

---
+++ tmux.sh
@@ -0,0 +1,8 @@
+sudo apt-get updatime
+sudo apt-get install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir ~/.config/tmux
+touch ~/.config/tmux/tmux.conf
+echo "set -g @plugin 'tmux-plugins/tpm'" > ~/.config/tmux/tmux.conf
+echo "run '~/.tmux/plugins/tpm/tpm'" >> ~/.config/tmux/tmux.conf
+ 
