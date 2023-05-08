59dcfe8e4d0ca63b7537250f0358cfa319928bc2
Lukas Wala <106024727+lswcomp@users.noreply.github.com>
2023-05-08 13:47:07 +0200

    Update neovim.sh
    Changed reference Path for nvim link to correct root directory

--- neovim.sh
+++ neovim.sh
@@ -2,6 +2,6 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 sudo ./nvim.appimage --appimage-extract
 sudo mv squashfs-root /
-sudo ln -s squashfs-root/AppRun /usr/local/bin/nvim
+sudo ln -s /squashfs-root/AppRun /usr/local/bin/nvim
 rm -rf ~/.local/share/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
